d89a910a4b9a8e0a1f0ac05e5b7c0c87255b1149
unknown <Lucas.Ferreira1859@gmail.com>
2023-04-20 14:49:57 -0300

    Finalizado: Relações de bit e byte, onde pode se dividir ou multiplicar por 8.

--- nova-func.py
+++ nova-func.py
@@ -13,3 +13,15 @@ def diferençaConversão(primeiraUnidade, unidadeParaConverter):
         contador += 1
     difDeConversão = primeiraUnidade - unidadeParaConverter
     return int(difDeConversão)
+
+def conversão(primeiraUnidade, unidadeParaConverter, númeroParaConversão):
+    difDeConversão = diferençaConversão(primeiraUnidade, unidadeParaConverter)
+    númeroConvertido = númeroParaConversão
+
+    if(primeiraUnidade == 'bit' or unidadeParaConverter == 'bit'):
+        if(primeiraUnidade == 'bit'):
+            númeroConvertido /= 8
+            difDeConversão += 1 
+        if(unidadeParaConverter == 'bit'):
+            númeroConvertido *= 8 
+            difDeConversão -= 1 
